c49794f0411c4e8c82a728c7db41d3ea3589e57a
Nolan Baker <nolan.lee.baker@gmail.com>
2015-05-03 17:21:07 -0500

    adds token manager

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# Cadence-Unity
+# Cadence
 payment processing, leaderboards, save states, etc for putting your Unity3D game in an arcade machine
+
+## TokenManager
+A MonoBehaviour that handles payment processing for tokens.
+
+### initialization
+`TokenManager.instance`
+
+### static fields
+`tokensPerCredit` - -n = 1/n tokens per credit = n credits per token, 0 = freeplay
+
+### static properties
+`tokensInserted` - total number of coins inserted so far
+`credits` - number of credits available for play
+
+### static events
+`onTokenInserted (int tokensSoFar, int tokensNeeded)`
+`onCreditAdded (int totalCredits)`
+`onCreditUsed (int totalCredits)`
+
+### static methods
+`void LoadSession ()` - loads tokensPerCredit, tokensInserted, and credits from a previous session
+`void SaveSession ()` - saves tokensPerCredit, tokensInserted, and credits for use in later sessions
+`void AddCredit (int credits)` - called when enough coins are inserted, call explicitly when awarding free plays
+`bool UseCredit ()` - call when the player presses start, returns false if there aren't enough credits
+
+## Leaderboards
+coming soon
+
+## Save States
+coming soon
---
+++ TokenManager.cs
@@ -0,0 +1,103 @@
+using UnityEngine;
+using System.Collections;
+using Paraphernalia.Utils;
+
+namespace Cadence {
+public class TokenManager : MonoBehaviour {
+
+	public delegate void OnTokenInserted(int tokensSoFar, int tokensNeeded);
+	public static event OnTokenInserted onTokenInserted = delegate{};
+
+	public delegate void OnCreditChanged(int totalCredits);
+	public static event OnCreditChanged onCreditAdded = delegate{};
+	public static event OnCreditChanged onCreditUsed = delegate{};
+
+	private static TokenManager _instance;
+	public static TokenManager instance {
+		get {
+			if (_instance == null) {
+				GameObject tokenMan = new GameObject("Cadence.TokenManager");
+				_instance = tokenMan.AddComponent<TokenManager>();
+				LoadSession();
+			}
+			return _instance;
+		}
+	}
+
+	[Tooltip("-n = 1/n tokens per credit = n credits per token, 0 = freeplay")]
+	public int tokensPerCredit = 1;
+
+	private int _tokensInserted = 0;
+	public int tokensInserted {
+		get { return _tokensInserted; }
+	}
+
+	private int _credits = 0;
+	public int credits {
+		get { return _credits; }
+	}
+	
+	void Awake () {
+		if (_instance == null) {
+			_instance = this;
+			LoadSession();
+		}
+		else if (_instance != this) {
+			Debug.LogWarning("TokenManager already initialized, destroying duplicate");
+			GameObjectUtils.Destroy(this);
+		}
+	}
+
+	public static void LoadSession () {
+		instance.tokensPerCredit = PlayerPrefs.GetInt("Cadence.tokensPerCredit", 1);
+		instance._tokensInserted = PlayerPrefs.GetInt("Cadence.tokensInserted", 0);
+		instance._credits = PlayerPrefs.GetInt("Cadence.credits", 0);
+	}
+
+	public static void SaveSession () {
+		PlayerPrefs.SetInt("Cadence.tokensPerCredit", instance.tokensPerCredit);
+		PlayerPrefs.SetInt("Cadence.tokensInserted", instance.tokensInserted);
+		PlayerPrefs.SetInt("Cadence.credits", instance.credits);
+	}
+
+	public void InsertToken() {
+		_tokensInserted++;
+		if (tokensPerCredit > 0) {
+			int tokensSoFar = tokensInserted % tokensPerCredit;
+			int tokensNeeded = tokensPerCredit - tokensSoFar;
+			onTokenInserted(tokensSoFar, tokensNeeded);
+			if (tokensSoFar == 0) AddCredit(1);
+		}
+		else if (tokensPerCredit < 0) {
+			AddCredit(-tokensPerCredit);
+		}
+		else {
+			Debug.LogError("Coin inserted during freeplay.");
+		}		
+	}
+
+	public static void AddCredit(int credits) {
+		instance._credits += credits;
+		onCreditAdded(instance.credits);
+	}
+
+	public static bool UseCredit() {
+		if (instance.credits > 0) {
+			instance._credits--;
+			onCreditUsed(instance.credits);
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	void Update () {
+		if (Input.GetButtonUp("Coin")) InsertToken();
+	}
+
+	void OnApplicationQuit() {
+        SaveSession();
+    }
+}
+}
