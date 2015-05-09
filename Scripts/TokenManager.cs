9a13a524e3c41bd6f6eece62a3cbe84f000de3a6
Nolan Baker <nolan.lee.baker@gmail.com>
2015-05-09 11:49:13 -0500

    adds token ui controller

--- Scripts/TokenManager.cs
+++ Scripts/TokenManager.cs
@@ -72,7 +72,7 @@ public class TokenManager : MonoBehaviour {
 			AddCredit(-tokensPerCredit);
 		}
 		else {
-			Debug.LogError("Coin inserted during freeplay.");
+			Debug.LogError("Token inserted during freeplay.");
 		}		
 	}
 
@@ -93,7 +93,7 @@ public class TokenManager : MonoBehaviour {
 	}
 
 	void Update () {
-		if (Input.GetButtonUp("Coin")) InsertToken();
+		if (Input.GetButtonUp("Token")) InsertToken();
 	}
 
 	void OnApplicationQuit() {
---
+++ Scripts/TokenUIControler.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Cadence {
+public class TokenUIControler : MonoBehaviour {
+
+	
+
+	void OnEnable() {
+		TokenManager.onTokenInserted += OnTokenInserted;
+		TokenManager.onCreditAdded += OnCreditAdded;
+		TokenManager.onCreditUsed += OnCreditUsed;
+	}
+
+	void OnDisable() {
+		TokenManager.onTokenInserted += OnTokenInserted;
+		TokenManager.onCreditAdded += OnCreditAdded;
+		TokenManager.onCreditUsed += OnCreditUsed;
+	}
+
+	void OnTokenInserted(int tokensSoFar, int tokensNeeded) {
+
+	}
+
+	void OnCreditAdded(int totalCredits) {
+
+	}
+
+	void OnCreditUsed(int totalCredits) {
+		
+	}
+}
+}
