289644c0e13fe7cd74014e21983dae2a16a23f21
Nolan Baker <nolan.lee.baker@gmail.com>
2015-05-14 21:51:01 -0500

    adds support for multiple coin acceptors

--- Scripts/TokenManager.cs
+++ Scripts/TokenManager.cs
@@ -4,10 +4,10 @@ using System.Collections;
 namespace Cadence {
 public class TokenManager : MonoBehaviour {
 
-	public delegate void OnTokenInserted(int tokensSoFar, int tokensNeeded);
+	public delegate void OnTokenInserted(int acceptor, int tokensSoFar, int tokensNeeded);
 	public static event OnTokenInserted onTokenInserted = delegate{};
 
-	public delegate void OnCreditChanged(int totalCredits);
+	public delegate void OnCreditChanged(int acceptor, int totalCredits);
 	public static event OnCreditChanged onCreditAdded = delegate{};
 	public static event OnCreditChanged onCreditUsed = delegate{};
 
@@ -16,8 +16,8 @@ public class TokenManager : MonoBehaviour {
 		get {
 			if (_instance == null) {
 				GameObject tokenMan = new GameObject("Cadence.TokenManager");
-				_instance = tokenMan.AddComponent<TokenManager>();
-				LoadSession();
+				tokenMan.AddComponent<TokenManager>();
+				Debug.Log(_instance);
 			}
 			return _instance;
 		}
@@ -25,18 +25,18 @@ public class TokenManager : MonoBehaviour {
 
 	[Tooltip("-n = 1/n tokens per credit = n credits per token, 0 = freeplay")]
 	public int tokensPerCredit = 1;
-	public KeyCode tokenKeyCode = KeyCode.Joystick1Button19;
+	[Range(0,19)] public int tokenButton = 19;
 	#if UNITY_EDITOR
-	public KeyCode altTokenKeyCode = KeyCode.T;
+	public KeyCode editorTokenKeyCode = KeyCode.T;
 	#endif
 	
-	private int _tokensInserted = 0;
-	public int tokensInserted {
+	private int[] _tokensInserted;
+	public int[] tokensInserted {
 		get { return _tokensInserted; }
 	}
 
-	private int _credits = 0;
-	public int credits {
+	private int[] _credits;
+	public int[] credits {
 		get { return _credits; }
 	}
 
@@ -52,6 +52,8 @@ public class TokenManager : MonoBehaviour {
 	void Awake () {
 		if (_instance == null) {
 			_instance = this;
+			_instance._tokensInserted = new int[CoinAcceptorCount()];
+			_instance._credits = new int[CoinAcceptorCount()];
 			DontDestroyOnLoad(_instance.gameObject);
 			LoadSession();
 		}
@@ -61,43 +63,55 @@ public class TokenManager : MonoBehaviour {
 		}
 	}
 
+	public static int JoystickCount () {
+		return Input.GetJoystickNames().Length;
+	}
+
+	public static int CoinAcceptorCount () {
+		return Mathf.Max(1, JoystickCount());
+	}
+
 	public static void LoadSession () {
 		instance.tokensPerCredit = PlayerPrefs.GetInt("Cadence.tokensPerCredit", 1);
-		instance._tokensInserted = PlayerPrefs.GetInt("Cadence.tokensInserted", 0);
-		instance._credits = PlayerPrefs.GetInt("Cadence.credits", 0);
+		for (int i = 0; i < CoinAcceptorCount(); i++) {
+			instance._tokensInserted[i] = PlayerPrefs.GetInt("Cadence.tokensInserted." + i, 0);
+			instance._credits[i] = PlayerPrefs.GetInt("Cadence.credits." + i, 0);
+		}
 	}
 
 	public static void SaveSession () {
 		PlayerPrefs.SetInt("Cadence.tokensPerCredit", instance.tokensPerCredit);
-		PlayerPrefs.SetInt("Cadence.tokensInserted", instance.tokensInserted);
-		PlayerPrefs.SetInt("Cadence.credits", instance.credits);
+		for (int i = 0; i < CoinAcceptorCount(); i++) {
+			PlayerPrefs.SetInt("Cadence.tokensInserted." + i, instance.tokensInserted[i]);
+			PlayerPrefs.SetInt("Cadence.credits." + i, instance.credits[i]);
+		}
 	}
 
-	public void InsertToken() {
-		_tokensInserted++;
+	public void InsertToken(int acceptor = 0) {
+		_tokensInserted[acceptor]++;
 		if (tokensPerCredit > 0) {
-			int tokensSoFar = tokensInserted % tokensPerCredit;
+			int tokensSoFar = tokensInserted[acceptor] % tokensPerCredit;
 			int tokensNeeded = tokensPerCredit - tokensSoFar;
-			onTokenInserted(tokensSoFar, tokensNeeded);
-			if (tokensSoFar == 0) AddCredit(1);
+			onTokenInserted(tokensSoFar, tokensNeeded, acceptor);
+			if (tokensSoFar == 0) AddCredit(1, acceptor);
 		}
 		else if (tokensPerCredit < 0) {
-			AddCredit(-tokensPerCredit);
+			AddCredit(-tokensPerCredit, acceptor);
 		}
 		else {
 			Debug.LogError("Token inserted during freeplay.");
 		}		
 	}
 
-	public static void AddCredit(int credits) {
-		instance._credits += credits;
-		onCreditAdded(instance.credits);
+	public static void AddCredit(int credits, int acceptor = 0) {
+		instance._credits[acceptor] += credits;
+		onCreditAdded(instance.credits[acceptor], acceptor);
 	}
 
-	public static bool UseCredit() {
-		if (instance.credits > 0) {
-			instance._credits--;
-			onCreditUsed(instance.credits);
+	public static bool UseCredit(int acceptor = 0) {
+		if (instance.credits[acceptor] > 0) {
+			instance._credits[acceptor]--;
+			onCreditUsed(instance.credits[acceptor], acceptor);
 			return true;
 		}
 		else {
@@ -105,7 +119,7 @@ public class TokenManager : MonoBehaviour {
 		}
 	}
 
-	public static string TokensPerCreditText (bool checkTokensInserted) {
+	public static string TokensPerCreditText (bool checkTokensInserted, int acceptor = 0) {
 		var tokensPerCredit = instance.tokensPerCredit;
 		if (tokensPerCredit == 0) {
 			return instance.freePlayString;
@@ -114,7 +128,7 @@ public class TokenManager : MonoBehaviour {
 			return instance.onePlayPerTokenString;
 		}	
 		else if (tokensPerCredit > 1) {
-			var tokensSoFar = instance.tokensInserted % instance.tokensPerCredit;
+			var tokensSoFar = instance.tokensInserted[acceptor] % instance.tokensPerCredit;
 			if (tokensSoFar == 0 || !checkTokensInserted) {
 				return string.Format(instance.coinsPerCreditFormatString, tokensPerCredit);
 			}
@@ -127,25 +141,28 @@ public class TokenManager : MonoBehaviour {
 		}	
 	}
 
-	public static string CreditsText () {
+	public static string CreditsText (int acceptor = 0) {
 		if (instance.tokensPerCredit == 0) {
 			return "";
 		}
 
-		if (instance.credits == 0) {
+		if (instance.credits[acceptor] == 0) {
 			return instance.insertCoinsString;
 		}
-		else if (instance.credits == 1) {
+		else if (instance.credits[acceptor] == 1) {
 			return instance.oneCreditString;
 		}
 		return string.Format(instance.nCreditsFormatString, instance.credits);
 	}
 
 	void Update () {
-		if (Input.GetKeyUp(tokenKeyCode)) InsertToken();
+		for (int i = 0; i < JoystickCount(); i++) {
+			string key = string.Format("joystick {0} button {1}", i, tokenButton);
+			if (Input.GetKeyUp(key)) InsertToken(i);
+		}
 
 		#if UNITY_EDITOR
-		if (Input.GetKeyUp(altTokenKeyCode)) InsertToken();
+		if (Input.GetKeyUp(editorTokenKeyCode)) InsertToken();
 		#endif
 	}
 
