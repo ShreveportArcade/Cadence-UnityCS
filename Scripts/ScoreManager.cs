21a52b63b83b8e606a1586411c8ad75671ed4b33
Nolan Baker <nolan.lee.baker@gmail.com>
2016-10-12 13:58:30 -0500

    scoreman uses variable game key instead of hardcoded VectorZ key

--- Scripts/ScoreManager.cs
+++ Scripts/ScoreManager.cs
@@ -5,6 +5,7 @@ namespace Cadence {
 public class ScoreManager : MonoBehaviour {
 
 	public static ScoreManager instance;
+	public string gameKey = "Game";
 
 	public static string[] initialsArray = new string[] {"---", "---", "---", "---", "---", "---"};
 	public static int[] scoresArray = new int[] { 100000, 8000, 6000, 4500, 3000, 1000 };
@@ -16,16 +17,17 @@ public class ScoreManager : MonoBehaviour {
 	
 	public static void LoadHighScores () {
 		for (int i = 0; i < 6; i++) {
-			initialsArray[i] = PlayerPrefs.GetString("VectorZ.HighScoreName" + i, "---");
-			scoresArray[i] = PlayerPrefs.GetInt("VectorZ.HighScore" + i, scoresArray[i]);
+			initialsArray[i] = PlayerPrefs.GetString(instance.gameKey + ".HighScoreName" + i, "---");
+			scoresArray[i] = PlayerPrefs.GetInt(instance.gameKey + ".HighScore" + i, scoresArray[i]);
 		}
 	}
 
 	public static void SaveHighScores () {
 		for (int i = 0; i < 6; i++) {
-			PlayerPrefs.SetString("VectorZ.HighScoreName" + i, initialsArray[i]);
-			PlayerPrefs.SetInt("VectorZ.HighScore" + i, scoresArray[i]);
+			PlayerPrefs.SetString(instance.gameKey + ".HighScoreName" + i, initialsArray[i]);
+			PlayerPrefs.SetInt(instance.gameKey + ".HighScore" + i, scoresArray[i]);
 		}
+		PlayerPrefs.Save();
 	}
 
 	public static bool CheckHighScore (int score) {
