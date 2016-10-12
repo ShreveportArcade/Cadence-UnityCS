14dea834174741d3da53cad503018bcf59aed48b
Nolan Baker <nolan.lee.baker@gmail.com>
2016-10-12 11:51:51 -0500

    namespaces scoreman

--- Scripts/ScoreManager.cs
+++ Scripts/ScoreManager.cs
@@ -1,37 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+namespace Cadence {
 public class ScoreManager : MonoBehaviour {
+
+	public static ScoreManager instance;
 
 	public static string[] initialsArray = new string[] {"---", "---", "---", "---", "---", "---"};
 	public static int[] scoresArray = new int[] { 100000, 8000, 6000, 4500, 3000, 1000 };
 
 	void Awake () {
+		if (instance == null) instance = this;
 		LoadHighScores();
 	}
 	
-	public void LoadHighScores () {
+	public static void LoadHighScores () {
 		for (int i = 0; i < 6; i++) {
 			initialsArray[i] = PlayerPrefs.GetString("VectorZ.HighScoreName" + i, "---");
 			scoresArray[i] = PlayerPrefs.GetInt("VectorZ.HighScore" + i, scoresArray[i]);
 		}
 	}
 
-	public void SaveHighScores () {
+	public static void SaveHighScores () {
 		for (int i = 0; i < 6; i++) {
 			PlayerPrefs.SetString("VectorZ.HighScoreName" + i, initialsArray[i]);
 			PlayerPrefs.SetInt("VectorZ.HighScore" + i, scoresArray[i]);
 		}
 	}
 
-	public bool CheckHighScore (int score) {
+	public static bool CheckHighScore (int score) {
 		for (int i = 0; i < 6; i++) {
 			if (score > scoresArray[i]) return true;
 		}
 		return false;
 	}
 
-	public void AddHighScore (int score, string initials) {
+	public static void AddHighScore (int score, string initials) {
 		int lastScore = 0;
 		string lastInitials = "";
 		bool highScoreSet = false;
@@ -48,4 +52,5 @@ public class ScoreManager : MonoBehaviour {
 		}
 		SaveHighScores();
 	}
+}
 }
