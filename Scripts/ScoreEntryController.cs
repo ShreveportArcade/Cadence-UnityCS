f60f9c6137d0df9e08cd8c6bd45ef482230cf783
Nolan Baker <nolan.lee.baker@gmail.com>
2017-07-26 09:53:02 -0500

    adds ScoreEntryController, default score entry prefab

---
+++ Scripts/ScoreEntryController.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using Paraphernalia.Components;
+using Cadence;
+
+public class ScoreEntryController : MonoBehaviour {
+
+	public delegate void OnNameSubmitted();
+	public static event OnNameSubmitted onNameSubmitted = delegate {};
+
+	public int numLetters = 3;
+	public Text[] letterSelectors;
+	public Button okButton;
+	public string selectSound = "select";
+	public string confirmSound = "confirm";
+
+	static char[] initials;
+	static int[] letterIndices;
+	int index = 0;
+	bool ready = false;
+
+	char[] letters = new char[] {
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
+		'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 
+		'U', 'V', 'W', 'X', 'Y', 'Z' ,
+		'@', '#', '$', '%', '^', '&', '*',
+		'|', '.', '!', '?', '-',
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+		'_'		
+	};
+
+	void OnEnable () {
+		StartCoroutine("Setup");
+	}
+
+	IEnumerator Setup () {
+		yield return new WaitForEndOfFrame();
+		yield return new WaitForEndOfFrame();
+		index = 0;
+		EventSystem.current.SetSelectedGameObject(letterSelectors[index].gameObject);
+		ready = true;
+	}
+
+	void Start () {
+		if (initials == null) {
+			initials = new char[] {'A', 'A', 'A'};
+			letterIndices = new int[initials.Length];
+		}
+
+		for (int i = 0; i < initials.Length; i++) {
+			letterSelectors[i].text = initials[i].ToString();
+		}
+	}
+
+	public void UpdateIndex (int dir) {
+		index = Mathf.Max(index + dir, 0);
+		if (index < letterSelectors.Length) {
+			EventSystem.current.SetSelectedGameObject(letterSelectors[index].gameObject);
+		}
+		else {
+			index = letterSelectors.Length;
+			EventSystem.current.SetSelectedGameObject(okButton.gameObject);
+		}
+	}
+
+	public void UpdateLetter (int dir) {
+		AudioManager.PlayEffect(selectSound);
+		letterIndices[index] = (letterIndices[index] + dir + letters.Length) % letters.Length;
+		char letter = letters[letterIndices[index]];
+		initials[index] = letter;
+		letterSelectors[index].text = letter.ToString();
+	}
+
+	public void NextPressed () {
+		AudioManager.PlayEffect(selectSound);
+		UpdateIndex(1);
+	}
+
+	public void OKPressed () {
+		ready = false;
+		AudioManager.PlayEffect(confirmSound);
+		ScoreManager.instance.AddHighScore(GameManager.instance.fliesEaten, new string(initials));
+		onNameSubmitted();
+	}
+
+	Vector2 lastDir;
+	void Update () {
+		if (!ready) return;
+
+		Vector2 dir = new Vector2(
+			Input.GetAxisRaw("Horizontal"), 
+			Input.GetAxisRaw("Vertical")
+		);
+
+		if (lastDir.magnitude < 0.5f && dir.magnitude >= 0.5f) {
+			if (Mathf.Abs(dir.x) > Mathf.Abs(dir.y)) UpdateIndex((int)Mathf.Sign(dir.x));
+			else UpdateLetter((int)Mathf.Sign(dir.y));
+		}
+
+		lastDir = dir;
+	}
+}
--- Scripts/ScoreManager.cs
+++ Scripts/ScoreManager.cs
@@ -8,15 +8,19 @@ public class ScoreManager : MonoBehaviour {
 	public string gameKey = "Game";
 
 	public string defaultInitials = "---";
-	public int[] defaultScoresArray = new int[] { 100000, 8000, 6000, 4500, 3000, 1000 };
+	public int[] defaultScores = new int[] { 100000, 8000, 6000, 4500, 3000, 1000 };
 
-	private string[] _initialsArray;
-	public string[] initialsArray {
-		get { return _initialsArray; }
+	private string[] _initials;
+	public string[] initials {
+		get { return _initials; }
 	}
-	private int[] _scoresArray;
-	public int[] scoresArray {
-		get { return _scoresArray; }
+	
+	private int[] _scores;
+	public int[] scores {
+		get { return _scores; }
+	}
+	public static int highScore {
+		get { return instance._scores[0]; }
 	}
 	
 	void Awake () {
@@ -26,30 +30,30 @@ public class ScoreManager : MonoBehaviour {
 	}
 
 	void InitializeScores () {
-		_initialsArray = new string[defaultScoresArray.Length];
-		for (int i = 0; i < defaultScoresArray.Length; i++) {
-			_initialsArray[i] = defaultInitials;
+		_initials = new string[defaultScores.Length];
+		for (int i = 0; i < defaultScores.Length; i++) {
+			_initials[i] = defaultInitials;
 		}
-		_scoresArray = defaultScoresArray.Clone() as int[];
+		_scores = defaultScores.Clone() as int[];
 	}
 	
 	public void LoadHighScores () {
-		for (int i = 0; i < defaultScoresArray.Length; i++) {
-			_initialsArray[i] = PlayerPrefs.GetString(gameKey + ".HighScoreName" + i, defaultInitials);
-			_scoresArray[i] = PlayerPrefs.GetInt(gameKey + ".HighScore" + i, defaultScoresArray[i]);
+		for (int i = 0; i < defaultScores.Length; i++) {
+			_initials[i] = PlayerPrefs.GetString(gameKey + ".HighScoreName" + i, defaultInitials);
+			_scores[i] = PlayerPrefs.GetInt(gameKey + ".HighScore" + i, defaultScores[i]);
 		}
 	}
 
 	public void SaveHighScores () {
-		for (int i = 0; i < defaultScoresArray.Length; i++) {
-			PlayerPrefs.SetString(gameKey + ".HighScoreName" + i, _initialsArray[i]);
-			PlayerPrefs.SetInt(gameKey + ".HighScore" + i, _scoresArray[i]);
+		for (int i = 0; i < defaultScores.Length; i++) {
+			PlayerPrefs.SetString(gameKey + ".HighScoreName" + i, _initials[i]);
+			PlayerPrefs.SetInt(gameKey + ".HighScore" + i, _scores[i]);
 		}
 		PlayerPrefs.Save();
 	}
 
 	public void ResetHighScores () {
-		for (int i = 0; i < defaultScoresArray.Length; i++) {
+		for (int i = 0; i < defaultScores.Length; i++) {
 			PlayerPrefs.DeleteKey(gameKey + ".HighScoreName" + i);
 			PlayerPrefs.DeleteKey(gameKey + ".HighScore" + i);
 		}
@@ -58,8 +62,8 @@ public class ScoreManager : MonoBehaviour {
 	}
 
 	public bool CheckHighScore (int score) {
-		for (int i = 0; i < defaultScoresArray.Length; i++) {
-			if (score > _scoresArray[i]) return true;
+		for (int i = 0; i < defaultScores.Length; i++) {
+			if (score > _scores[i]) return true;
 		}
 		return false;
 	}
@@ -68,13 +72,13 @@ public class ScoreManager : MonoBehaviour {
 		int lastScore = 0;
 		string lastInitials = "";
 		bool highScoreSet = false;
-		for (int i = 0; i < defaultScoresArray.Length; i++) {
-			if (score > _scoresArray[i] || highScoreSet) {
+		for (int i = 0; i < defaultScores.Length; i++) {
+			if (score > _scores[i] || highScoreSet) {
 				highScoreSet = true;
-				lastScore = _scoresArray[i];
-				lastInitials = _initialsArray[i];
-				_scoresArray[i] = score;
-				_initialsArray[i] = initials;
+				lastScore = _scores[i];
+				lastInitials = _initials[i];
+				_scores[i] = score;
+				_initials[i] = initials;
 				score = lastScore;
 				initials = lastInitials;
 			}
