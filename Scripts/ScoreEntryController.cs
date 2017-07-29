6112b5a8dcc0979c39dd04a1a7059de0cb178ff2
Nolan Baker <nolan.lee.baker@gmail.com>
2017-07-28 23:15:41 -0500

    adds trackball entry

--- Scripts/ScoreEntryController.cs
+++ Scripts/ScoreEntryController.cs
@@ -11,6 +11,12 @@ public class ScoreEntryController : MonoBehaviour {
 	public delegate void OnNameSubmitted(string name);
 	public static event OnNameSubmitted onNameSubmitted = delegate {};
 
+	public enum EntryType {
+		Joystick,
+		Trackball
+	}
+	public EntryType entryType = EntryType.Joystick;
+
 	public int numLetters = 3;
 	public Text[] letterSelectors;
 	public Button okButton;
@@ -90,10 +96,24 @@ public class ScoreEntryController : MonoBehaviour {
 	void Update () {
 		if (!ready) return;
 
-		Vector2 dir = new Vector2(
-			Input.GetAxisRaw("Horizontal"), 
-			Input.GetAxisRaw("Vertical")
-		);
+		Vector2 dir = Vector2.zero;
+		
+		switch (entryType) {
+			case EntryType.Joystick:
+				dir.x = Input.GetAxisRaw("Horizontal");
+				dir.y = Input.GetAxisRaw("Vertical");
+				break;
+			case EntryType.Trackball:
+				if (Input.GetMouseButtonDown(0)) {
+					if (index < letterIndices.Length) NextPressed();
+					else OKPressed();
+				}
+				dir.x = Input.GetAxisRaw("Mouse X");
+				dir.y = Input.GetAxisRaw("Mouse Y");
+				break;
+			default:
+				break;
+		}
 
 		if (lastDir.magnitude < 0.5f && dir.magnitude >= 0.5f) {
 			if (Mathf.Abs(dir.x) > Mathf.Abs(dir.y)) UpdateIndex((int)Mathf.Sign(dir.x));
